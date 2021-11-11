660a1340d7c062fa16811cfe5642a4b6e7f3378c
Hypecolt <59376021+Hypecolt@users.noreply.github.com>
2021-11-11 19:59:05 +0200

    Add files via upload

---
+++ ocry.py
@@ -0,0 +1,74 @@
+class ocry():
+	def __init__(self):
+		self.btc = 0
+		self.eth = 0
+		self.xrp = 0
+		self.eos = 0
+		self.ltc = 0
+		self.trx = 0
+		self.xlm = 0
+		self.zrx = 0
+		self.doge = 0
+	
+	#btc
+	def setbtc(self, amount):
+		self.btc = self.btc + amount
+	
+	def getbtc(self):
+		return self.btc
+		
+	#eth
+	def seteth(self, amount):
+		self.eth = self.eth + amount
+	
+	def geteth(self):
+		return self.eth
+		
+	#xrp
+	def setxrp(self, amount):
+		self.xrp = self.xrp + amount
+	
+	def getxrp(self):
+		return self.xrp
+		
+	#eos
+	def seteos(self, amount):
+		self.eos = self.eos + amount
+	
+	def geteos(self):
+		return self.eos
+		
+	#ltc
+	def setltc(self, amount):
+		self.ltc = self.ltc + amount
+	
+	def getltc(self):
+		return self.ltc
+		
+	#trx
+	def settrx(self, amount):
+		self.trx = self.trx + amount
+	
+	def gettrx(self):
+		return self.trx
+		
+	#xlm
+	def setxlm(self, amount):
+		self.xlm = self.xlm + amount
+	
+	def getxlm(self):
+		return self.xlm
+		
+	#zrx
+	def setzrx(self, amount):
+		self.zrx = self.zrx + amount
+	
+	def getzrx(self):
+		return self.zrx
+		
+	#doge
+	def setdoge(self, amount):
+		self.doge = self.doge + amount
+	
+	def getdoge(self):
+		return self.doge
